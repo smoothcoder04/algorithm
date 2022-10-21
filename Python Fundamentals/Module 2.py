f50971e6377953e23a59c2a82abedffbc7a3a2db
Mithu <mithu.mawhinney@vodafone.com>
2022-10-21 14:32:11 +0100

    Arithmatic operators in module 2

--- Python Fundamentals/Module 2.py
+++ Python Fundamentals/Module 2.py
@@ -1,1 +1,25 @@
 # Arithmatic operators
+
+"""
+The operatores who have same precedence when encountered will be execurted from left side first.
+"""
+
+
+def arith():
+    print(3*4/5)
+    print(3/4*5)
+
+
+arith()
+
+
+"""
+Booleans - in bit terms True is 1 & False is 2
+"""
+
+
+def arith2():
+    print((20+True)/(4-5*False))
+
+
+arith2()
